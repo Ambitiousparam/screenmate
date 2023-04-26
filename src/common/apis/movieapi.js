1372caa209584446946bd72b3d2ad38293528b44
parampreet singh <parampreetsingh1092@gmail.com>
2023-04-26 21:52:11 +0530

    first commit

---
+++ src/common/apis/movieapi.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+
+
+
+
+
+export default axios.create({
+    baseurl :"https://www.omdbapi.com",
+});
---
+++ src/components/Moviedetails/Moviedetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Moviedetails = () => {
+    return (
+        <div>
+            MovieDetails
+        </div>
+    );
+};
+
+export default Moviedetails;
